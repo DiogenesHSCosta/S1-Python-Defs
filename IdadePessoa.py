a1e5be6dbe05c0257e2e0be8ff82d34be255b0af
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-07 20:04:03 -0300

    Utilização de função dentro de função

--- IdadePessoa.py
+++ IdadePessoa.py
@@ -1,9 +1,17 @@
-def idade(nome, idade):
-    idade = 2024 - idade
-    print(f'idade do(a) {nome} é {idade}')
+
+def podeVotar(nome, idade):
+    idade = calculaIdade(idade)
+    if idade >= 16:
+        print(f"Sim, a pessoa, {nome} pode votar")
+        return True
+    else:
+        print(f'Não, a pessoa, {nome} ainda n pode votar')
+        return False
+def calculaIdade(ano):
+    idade = 2024 - ano
     return idade
 
 
-pessoa = idade('douglas', 2004)
+pessoa = podeVotar('douglas', 2004)
 
 print(pessoa)
