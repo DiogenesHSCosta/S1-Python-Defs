479c7c473d62b0cbdb4d05169cdd2c7e1f68da79
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-07 21:34:58 -0300

    melhoria qualidade de leitura código

--- CalculaMinuto.py
+++ CalculaMinuto.py
@@ -1,16 +1,18 @@
-
 def proximoMinuto(hora, min):
-    if hora >23 or min >59:
+    if hora > 23 or min > 59:
         return 'Insira uma hora valida'
+
     elif min != 59:
-        return f'{hora}:{min+1}'
+        return f'{hora}:{min + 1}'
 
     elif hora == 23 and min == 59:
-        return f'0{0}:0{0}'
+        return f'00:00'
 
     elif hora != 23 and min == 59:
-        return f'{hora+1}:0{0}'
-
-print(proximoMinuto(22,59))
+        return f'{hora + 1}:0{0}'
 
 
+print(proximoMinuto(22, 59))
+print(proximoMinuto(1, 23))
+print(proximoMinuto(12, 2))
+print(proximoMinuto(20, 59))
